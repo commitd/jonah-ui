f6fbf85b7306722bbef77bb5ae7a4851bb1f6d8b
Chris Flatley <chris@committed.io>
2018-02-02 16:19:37 +0000

    Attempt to deal with general metadata values

--- packages/ketos-components/src/ketos/document/DocumentMeta.tsx
+++ packages/ketos-components/src/ketos/document/DocumentMeta.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import { Table } from 'semantic-ui-react'
+import { Ellipsis } from 'invest-components'
 
 export interface Metadata {
     key: string
@@ -13,6 +14,18 @@ export interface OwnProps {
 export type Props = OwnProps
 
 class DocumentMeta extends React.Component<Props> {
+
+    renderSafeValue(v: {}): React.ReactElement<{}> {
+        if (v == null) {
+            return <span />
+        } else if (v instanceof Array) {
+            const l = v as Array<{}>
+            return <div>{l.map(this.renderSafeValue)}</div>
+        } else {
+            const s = v.toString()
+            return <Ellipsis key={s} text={s} />
+        }
+    }
 
     render() {
         const { metadata } = this.props
@@ -33,7 +46,7 @@ class DocumentMeta extends React.Component<Props> {
                     {
                         metadata.map(m => <Table.Row key={m.key}>
                             <Table.Cell>{m.key}</Table.Cell>
-                            <Table.Cell>{m.value.map(s => <span key={s}>{s})</span>)}</Table.Cell>
+                            <Table.Cell>{this.renderSafeValue(m.value)}</Table.Cell>
                         </Table.Row>
                         )}
                 </Table.Body>
