9935ff771a99551b611ea69f653d4993973cd794
Chris Flatley <chris@committed.io>
2018-02-04 14:42:48 +0000

    Replicate relationexplorer into a network and details focus projects

---
+++ packages/ketos-ui-relationdetails/src/App.tsx
@@ -0,0 +1,81 @@
+import * as React from 'react'
+const isEqual = require('lodash.isequal')
+import { ChildProps } from 'invest-plugin'
+import DataContainer from './DataContainer'
+import RelationView from './RelationView'
+import { MessageBox } from 'ketos-components'
+type OwnProps = {}
+
+type Props = OwnProps & ChildProps
+
+type State = {
+  datasetId?: string,
+  entityId?: string,
+  relationId?: string
+}
+
+type RelationViewPayload = {
+  dataset: string
+  relationId: string
+}
+
+type EntityViewPayload = {
+  dataset: string
+  entityId: string
+}
+
+class App extends React.Component<Props, State> {
+
+  state: State = {
+    datasetId: 're3d',
+    entityId: '5a5dc9fc793439d353c3874b'
+  }
+
+  componentWillReceiveProps(nextProps: Props) {
+    if (this.props.action !== nextProps.action || !isEqual(this.props.payload, nextProps.payload)) {
+      this.onAction(nextProps.action, nextProps.payload)
+    }
+  }
+
+  render() {
+    const { entityId, datasetId } = this.state
+
+    if (datasetId == null || entityId == null) {
+      return (
+        <MessageBox
+          title="No entity or dataset"
+          description="Please use another plugin to select an entity of dataset"
+        />
+      )
+    }
+
+    return (
+      <DataContainer variables={{ datasetId: datasetId, entityId: entityId }} >
+        <RelationView />
+      </DataContainer >
+
+    )
+  }
+
+  private onAction = (action?: string, payload?: {}) => {
+
+    if (action === 'relation.view') {
+      const p = payload as RelationViewPayload
+      this.setState({
+        datasetId: p.dataset,
+        relationId: p.relationId,
+        entityId: undefined
+      })
+    } else if (action === 'entity.view') {
+      const p = payload as EntityViewPayload
+      this.setState({
+        datasetId: p.dataset,
+        entityId: p.entityId,
+        relationId: undefined
+      })
+    }
+
+  }
+}
+
+export default App
---
+++ packages/ketos-ui-relationdetails/src/DataContainer.tsx
@@ -0,0 +1,93 @@
+import gql from 'graphql-tag'
+
+import { createDataContainer } from 'ketos-components'
+
+type Variables = {
+    datasetId: string
+    entityId: string
+}
+
+export type Response = {
+    corpus: {
+        entity: {
+            id: string,
+            docId: string,
+            type: string,
+            longestValue: string,
+            mentions: {
+                id: string
+                type: string,
+                value: string,
+                targetOf: {
+                    id: string
+                    relationshipType: string
+                    relationSubtype: string
+                    source: {
+                        id: string
+                        value: string
+                        type: string
+                        entityId: string
+                    }
+                }[]
+                sourceOf: {
+                    id: string
+                    relationshipType: string
+                    relationSubtype: string
+                    type: string
+                    target: {
+                        id: string
+                        value: string
+                        type: string
+                        entityId: string
+                    }
+                }[]
+            }[]
+        }
+    }
+}
+
+export const GET_RELATIONS_FOR_ENTITY_QUERY = `
+query GetRelationsForEntity($datasetId: String!, $entityId: ID) {
+    corpus(id: $datasetId) {
+      entity(id: $entityId) {
+        id
+        docId
+        type
+        longestValue
+        mentions {
+          id
+          type
+          value
+          targetOf {
+            id
+            relationshipType
+            relationSubtype 
+            source {
+              id
+              value
+              type
+              entityId
+            }
+          }
+          sourceOf {
+            id
+            relationshipType
+            relationSubtype
+            type 
+            target {
+              id
+              value
+              type
+              entityId
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+export const QUERY = gql(GET_RELATIONS_FOR_ENTITY_QUERY)
+
+const dataContainer = createDataContainer<Variables, Response>(QUERY)
+export default dataContainer
