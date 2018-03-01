94fddf2ba2e5b1623215ff14d6c49df41c835ac5
Chris Flatley <chris@committed.io>
2018-03-01 13:00:57 +0000

    Very limited support for non string properties

--- packages/ketos-ui-editor/src/components/PropertiesEditor.tsx
+++ packages/ketos-ui-editor/src/components/PropertiesEditor.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { PropertiesMap } from 'invest-types'
-import { Form } from 'semantic-ui-react'
+import { Form, Input, TextArea } from 'semantic-ui-react'
 import update from 'immutability-helper'
 
 export type Props = {
@@ -46,13 +46,7 @@ export default class PropertiesEditor extends React.Component<Props> {
                     readOnly={!edit}
                     width={6}
                 />
-                <Form.Input
-                    label="Value"
-                    value={value || ''}
-                    onChange={(e, data) => this.handleValueChanged(key, data.value)}
-                    readOnly={!edit}
-                    width={8}
-                />
+                {this.renderValue(key, value, edit)}
                 {edit && <Form.Button
                     width={2}
                     negative={true}
@@ -62,6 +56,44 @@ export default class PropertiesEditor extends React.Component<Props> {
                     disabled={!edit}
                 />}
             </Form.Group>
+        )
+    }
+
+    private renderValue = (key: string, value: {}, edit: boolean) => {
+        let control: {} = Input
+        let formValue: {}
+        let formType = 'text'
+        let type = 'string'
+        if (value instanceof Array || value instanceof Object) {
+            control = TextArea
+            type = 'json'
+            formValue = JSON.stringify(value)
+        } else if (typeof value === 'number'
+            && (key === 'documentDate' || key === 'timestamp' || key === 'timestampStart' || key === 'timestampStop')) {
+            const s = new Date(value as number).toISOString()
+            formValue = s.substring(0, s.length - 1)
+            formType = 'datetime-local'
+            type = 'datetime'
+        } else if (typeof value === 'number') {
+            formType = 'number'
+            formValue = value
+            type = 'number'
+        } else {
+            formValue = value || ''
+        }
+
+        return (
+            <Form.Field
+                type={formType}
+                control={control}
+                label={type === 'json' ? <span>Value<br /><small>(Paste only)</small></span> : 'Value'}
+                value={formValue}
+                onChange={(e: {}, data: { value: {} }) => this.handleValueChanged(key, data.value, type)}
+                readOnly={!edit
+                }
+                width={8}
+                warning={true}
+            />
         )
     }
 
@@ -84,7 +116,26 @@ export default class PropertiesEditor extends React.Component<Props> {
         )
     }
 
-    private handleValueChanged = (key: string, value: {}) => {
+    private handleValueChanged = (key: string, newValue: {}, type: string) => {
+        console.log(newValue)
+        let value: {}
+        switch (type) {
+            case 'json':
+                try {
+                    value = JSON.parse(newValue as string)
+                } catch (e) {
+                    console.warn('Only copy and paste is supported')
+                    return
+                }
+                break
+            case 'datetime':
+                value = new Date(newValue as string).getTime()
+                break
+            default:
+                value = newValue
+                break
+        }
+
         this.props.onChange(
             update(this.props.properties || {}, { $merge: { [key]: value } })
         )
