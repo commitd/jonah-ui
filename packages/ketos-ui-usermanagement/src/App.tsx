49cf5f0c84f53aaf58feb72e157fc840fd461c44
Jonathan Elliot <jon@committed.io>
2018-03-23 16:14:32 +0000

    User management plugin boilerplate

---
+++ packages/ketos-ui-usermanagement/src/App.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react'
+const isEqual = require('lodash.isequal')
+
+import { PluginProps } from 'invest-plugin'
+
+type OwnProps = {}
+
+type Props = OwnProps & PluginProps
+
+class App extends React.Component<Props> {
+
+  componentWillReceiveProps(nextProps: Props) {
+    if (this.props.action !== nextProps.action || !isEqual(this.props.payload, nextProps.payload)) {
+      this.onAction(nextProps.action, nextProps.payload)
+    }
+  }
+
+  render() {
+    return (
+      <p>Hello world</p>
+    )
+  }
+
+  private onAction = (action?: string, payload?: {}) => {
+    // Implement to deal with new action requests
+    // typically this will setState in order and then pass
+    // that state as props to a subcomponent (which will
+    // then respond with a )
+  }
+}
+
+export default App
