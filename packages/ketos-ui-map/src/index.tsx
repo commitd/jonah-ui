3129986b93d44ac442e66e630b9220e556031e06
Chris Flatley <chris@committed.io>
2018-03-26 14:36:11 +0100

    Remove serviceworker

--- packages/ketos-ui-map/src/index.tsx
+++ packages/ketos-ui-map/src/index.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import App from './App'
-import registerServiceWorker from './registerServiceWorker'
 import './index.css'
 
 import 'semantic-ui-offline/semantic.min.css'
@@ -42,4 +41,3 @@ ReactDOM.render(
   </InvestUiPlugin>,
   document.getElementById('root') as HTMLElement
 )
-registerServiceWorker()
