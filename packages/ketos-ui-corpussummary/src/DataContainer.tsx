47276bf41a12c5a5037cc0c62b15c4e19c47228f
Chris Flatley <chris@committed.io>
2018-05-16 10:17:52 +0100

    Make corpus summary responsive

--- packages/ketos-ui-corpussummary/src/DataContainer.tsx
+++ packages/ketos-ui-corpussummary/src/DataContainer.tsx
@@ -2,90 +2,91 @@ import gql from 'graphql-tag'
 import { createDataContainer } from 'invest-components'
 
 type Variables = {
-    datasetId: string
+  datasetId: string
 }
 
 export type Response = {
-    corpus: {
-        id: string
-        name: string
-        countDocuments?: number
-        countEntities?: number
-        countMentions?: number
-        countRelations?: number
-        documentTypes: {
-            bins: {
-                count: number
-                term: string
-            }[]
-        }
-        documentLanguages: {
-            bins: {
-                count: number
-                term: string
-            }[]
-        }
-        documentClassifications: {
-            bins: {
-                count: number
-                term: string
-            }[]
-        }
-        mentionTypes: {
-            bins: {
-                count: number
-                term: string
-            }[]
-        }
-        documentTimeline: {
-            bins: {
-                ts: string
-                count: number
-            }[]
-        }
+  corpus: {
+    id: string
+    name: string
+    countDocuments?: number
+    countEntities?: number
+    countMentions?: number
+    countRelations?: number
+    documentTypes: {
+      bins: {
+        count: number
+        term: string
+      }[]
     }
-}
-
-const QUERY = gql`
-query CorpusInfo($datasetId: String!)  {
-  corpus(id:$datasetId) {
-    id
-    name
-    countDocuments
-    countEntities
-    countRelations
-    documentTypes: countByDocumentField(field:"properties.type") {
-        bins {
-            count
-            term
-        }
+    documentLanguages: {
+      bins: {
+        count: number
+        term: string
+      }[]
     }
-    documentLanguages: countByDocumentField(field:"properties.language") {
-        bins {
-            count
-            term
-        }
+    documentClassifications: {
+      bins: {
+        count: number
+        term: string
+      }[]
     }
-    documentClassifications: countByDocumentField(field:"properties.classification")  {
-        bins {
-            count
-            term
-        }
+    mentionTypes: {
+      bins: {
+        count: number
+        term: string
+      }[]
     }
-    mentionTypes: countByMentionField(field:"properties.type")  {
-        bins {
-            count
-            term
-        }
-    }
-    documentTimeline: documentTimeline {
-        bins {
-            ts
-            count
-        }
+    documentTimeline: {
+      bins: {
+        ts: string
+        count: number
+      }[]
     }
   }
 }
+
+const QUERY = gql`
+  query CorpusInfo($datasetId: String!) {
+    corpus(id: $datasetId) {
+      id
+      name
+      countDocuments
+      countEntities
+      countRelations
+      countMentions
+      documentTypes: countByDocumentField(field: "properties.type") {
+        bins {
+          count
+          term
+        }
+      }
+      documentLanguages: countByDocumentField(field: "properties.language") {
+        bins {
+          count
+          term
+        }
+      }
+      documentClassifications: countByDocumentField(field: "properties.classification") {
+        bins {
+          count
+          term
+        }
+      }
+      mentionTypes: countByMentionField(field: "properties.type") {
+        bins {
+          count
+          term
+        }
+      }
+      documentTimeline: documentTimeline {
+        bins {
+          ts
+          count
+        }
+      }
+    }
+  }
 `
 
 export default createDataContainer<Variables, Response>(QUERY)
--- packages/ketos-ui-corpussummary/src/View.tsx
+++ packages/ketos-ui-corpussummary/src/View.tsx
@@ -1,101 +1,127 @@
+import { BarChart, Card, Counter, PieChart, TimelineChart, termBinsToXY, timeBinsToXY } from 'invest-components'
 import * as React from 'react'
-
 import { Grid } from 'semantic-ui-react'
-import { Counter, PieChart, BarChart, TimelineChart, Card, termBinsToXY, timeBinsToXY } from 'invest-components'
-
 import { Response } from './DataContainer'
 
 type OwnProps = {
-    data?: Response
+  data?: Response
 }
 
 type Props = OwnProps
 
 class View extends React.Component<Props> {
+  render() {
+    const { data } = this.props
 
-    render() {
-
-        const { data } = this.props
-
-        if (!data || !data.corpus) {
-            return 'No data'
-        }
-
-        const numDocuments = data.corpus.countDocuments
-        const numMentions = data.corpus.countMentions
-        const numEntities = data.corpus.countEntities
-        const numRelations = data.corpus.countRelations
-        const documentTypes = data.corpus.documentTypes.bins
-        const documentLanguages = data.corpus.documentLanguages.bins
-        const documentClassifications = data.corpus.documentClassifications.bins
-        const mentionTypes = data.corpus.mentionTypes.bins
-        const documentTimeline = data.corpus.documentTimeline.bins.map(b => ({ ts: Date.parse(b.ts), count: b.count }))
-
-        const numEvents = null
-
-        return (
-            <div >
-                <Grid>
-                    <Grid.Row columns={5}>
-                        {numDocuments != null && <Grid.Column>
-                            <Counter value={numDocuments || 0} singular="document" plural="documents" />
-                        </Grid.Column>}
-                        {numMentions != null && <Grid.Column>
-                            <Counter value={numMentions || 0} singular="mention" plural="mentions" />
-                        </Grid.Column>}
-                        {numEntities != null && <Grid.Column>
-                            <Counter value={numEntities || 0} singular="entity" plural="entities" />
-                        </Grid.Column>}
-                        {numRelations != null && <Grid.Column>
-                            <Counter value={numRelations || 0} singular="relation" plural="relations" />
-                        </Grid.Column>}
-                        {numEvents != null && <Grid.Column>
-                            <Counter value={numEvents || 0} singular="event" plural="events" />
-                        </Grid.Column>}
-                    </Grid.Row>
-                </Grid>
-                <Grid>
-                    <Grid.Row columns={1}>
-                        {documentTimeline && documentTimeline.length > 1 && <Grid.Column>
-                            <Card title="Document timeline">
-                                <TimelineChart data={timeBinsToXY(documentTimeline)} />
-                            </Card>
-                        </Grid.Column>}
-                    </Grid.Row>
-                </Grid>
-                <Grid><Grid.Row columns={3}>
-                    {documentTypes && <Grid.Column>
-                        <Card title="Types">
-                            {documentTypes.length > 0 ?
-                                <PieChart data={termBinsToXY(documentTypes)} /> : <p><b>No data</b></p>}
-                        </Card>
-                    </Grid.Column>}
-                    {documentLanguages && <Grid.Column>
-                        <Card title="Languages">
-                            {documentLanguages.length > 0 ?
-                                <PieChart data={termBinsToXY(documentLanguages)} /> : <p><b>No data</b></p>}
-                        </Card>
-                    </Grid.Column>}
-                    {documentClassifications && <Grid.Column>
-                        <Card title="Classifications">
-                            {documentClassifications.length > 0 ?
-                                <PieChart data={termBinsToXY(documentClassifications)} /> : <p><b>No data</b></p>}
-                        </Card>
-                    </Grid.Column>}
-                </Grid.Row>
-                </Grid>
-                {mentionTypes && <Grid><Grid.Row columns={1}><Grid.Column>
-                    <Card
-                        title="Mention types"
-                        subTitle={`${mentionTypes.length} mention types within the corpus`}
-                    >
-                        <BarChart data={termBinsToXY(mentionTypes)} />
-                    </Card>
-                </Grid.Column></Grid.Row></Grid>}
-
-            </div>
-        )
+    if (!data || !data.corpus) {
+      return 'No data'
     }
+
+    const numDocuments = data.corpus.countDocuments
+    const numMentions = data.corpus.countMentions
+    const numEntities = data.corpus.countEntities
+    const numRelations = data.corpus.countRelations
+    const documentTypes = data.corpus.documentTypes.bins
+    const documentLanguages = data.corpus.documentLanguages.bins
+    const documentClassifications = data.corpus.documentClassifications.bins
+    const mentionTypes = data.corpus.mentionTypes.bins
+    const documentTimeline = data.corpus.documentTimeline.bins.map(b => ({ ts: Date.parse(b.ts), count: b.count }))
+
+    return (
+      <div>
+        <Grid>
+          <Grid.Row stretched={true}>
+            {numDocuments != null && (
+              <Grid.Column computer={4} tablet={8} mobile={16}>
+                <Counter value={numDocuments || 0} singular="document" plural="documents" />
+              </Grid.Column>
+            )}
+            {numMentions != null && (
+              <Grid.Column computer={4} tablet={8} mobile={16}>
+                <Counter value={numMentions || 0} singular="mention" plural="mentions" />
+              </Grid.Column>
+            )}
+            {numEntities != null && (
+              <Grid.Column computer={4} tablet={8} mobile={16}>
+                <Counter value={numEntities || 0} singular="entity" plural="entities" />
+              </Grid.Column>
+            )}
+            {numRelations != null && (
+              <Grid.Column computer={4} tablet={8} mobile={16}>
+                <Counter value={numRelations || 0} singular="relation" plural="relations" />
+              </Grid.Column>
+            )}
+          </Grid.Row>
+        </Grid>
+        <Grid>
+          <Grid.Row columns={1}>
+            {documentTimeline &&
+              documentTimeline.length > 1 && (
+                <Grid.Column>
+                  <Card title="Document timeline">
+                    <TimelineChart data={timeBinsToXY(documentTimeline)} />
+                  </Card>
+                </Grid.Column>
+              )}
+          </Grid.Row>
+        </Grid>
+        <Grid>
+          <Grid.Row columns={3} centered={true}>
+            {documentTypes && (
+              <Grid.Column computer={5} tablet={16} mobile={16}>
+                <Card title="Types">
+                  {documentTypes.length > 0 ? (
+                    <PieChart data={termBinsToXY(documentTypes)} />
+                  ) : (
+                    <p>
+                      <b>No data</b>
+                    </p>
+                  )}
+                </Card>
+              </Grid.Column>
+            )}
+            {documentLanguages && (
+              <Grid.Column computer={5} tablet={16} mobile={16}>
+                <Card title="Languages">
+                  {documentLanguages.length > 0 ? (
+                    <PieChart data={termBinsToXY(documentLanguages)} />
+                  ) : (
+                    <p>
+                      <b>No data</b>
+                    </p>
+                  )}
+                </Card>
+              </Grid.Column>
+            )}
+            {documentClassifications && (
+              <Grid.Column computer={5} tablet={16} mobile={16}>
+                <Card title="Classifications">
+                  {documentClassifications.length > 0 ? (
+                    <PieChart data={termBinsToXY(documentClassifications)} />
+                  ) : (
+                    <p>
+                      <b>No data</b>
+                    </p>
+                  )}
+                </Card>
+              </Grid.Column>
+            )}
+          </Grid.Row>
+        </Grid>
+        {mentionTypes && (
+          <Grid>
+            <Grid.Row columns={1}>
+              <Grid.Column>
+                <Card title="Mention types" subTitle={`${mentionTypes.length} mention types within the corpus`}>
+                  <BarChart data={termBinsToXY(mentionTypes)} />
+                </Card>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        )}
+      </div>
+    )
+  }
 }
 
 export default View
