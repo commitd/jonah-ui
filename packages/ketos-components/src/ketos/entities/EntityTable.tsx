a09183d5bfd86a1ed332711c1a18946f4396d5f2
Chris Flatley <chris@committed.io>
2018-05-16 13:13:06 +0100

    fix ENTITY_SEARCH payloads in callers

--- packages/ketos-components/src/ketos/entities/EntityTable.tsx
+++ packages/ketos-components/src/ketos/entities/EntityTable.tsx
@@ -1,94 +1,83 @@
+import { ActionDropdown, Paginated } from 'invest-components'
 import * as React from 'react'
 import { Table } from 'semantic-ui-react'
-import { ActionDropdown } from 'invest-components'
+import { ENTITY_SEARCH, ENTITY_VIEW, MENTION_SEARCH } from '../../Actions'
 import { BasicEntityNode } from '../../types'
-import { ENTITY_VIEW, ENTITY_SEARCH, MENTION_SEARCH } from '../../Actions'
-import { Paginated } from 'invest-components'
 
 export interface Props {
-    datasetId: string,
-    entities: BasicEntityNode[],
-    selectedIds?: string[]
+  datasetId: string
+  entities: BasicEntityNode[]
+  selectedIds?: string[]
 }
 
 class EntityTable extends React.Component<Props> {
+  render() {
+    const { entities, selectedIds } = this.props
 
-    render() {
-        const { entities, selectedIds } = this.props
+    const selected = selectedIds != null ? selectedIds : []
 
-        const selected = selectedIds != null ? selectedIds : []
+    return (
+      <div>
+        <Table>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>Type</Table.HeaderCell>
+              <Table.HeaderCell>Value</Table.HeaderCell>
+              <Table.HeaderCell />
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {entities.map(m => (
+              <Table.Row key={m.id} positive={selected.includes(m.id)}>
+                <Table.Cell>{m.type}</Table.Cell>
+                <Table.Cell>{m.value}</Table.Cell>
+                <Table.Cell>
+                  <ActionDropdown
+                    text="View"
+                    actions={[ENTITY_VIEW, ENTITY_SEARCH, MENTION_SEARCH]}
+                    onSelect={this.handleAction(m)}
+                  />
+                </Table.Cell>
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </Table>
+      </div>
+    )
+  }
 
-        return (
-            <div>
-                <Table>
-                    <Table.Header>
-                        <Table.Row>
-                            <Table.HeaderCell>
-                                Type
-                            </Table.HeaderCell>
-                            <Table.HeaderCell>
-                                Value
-                            </Table.HeaderCell>
-                            <Table.HeaderCell />
-                        </Table.Row>
-                    </Table.Header>
-                    <Table.Body>
-                        {
-                            entities.map(m =>
-                                <Table.Row key={m.id} positive={selected.includes(m.id)} >
-                                    <Table.Cell>
-                                        {m.type}
-                                    </Table.Cell>
-                                    <Table.Cell>
-                                        {m.value}
-                                    </Table.Cell>
-                                    <Table.Cell>
-                                        <ActionDropdown
-                                            text="View"
-                                            actions={[ENTITY_VIEW, ENTITY_SEARCH,
-                                                MENTION_SEARCH]}
-                                            onSelect={this.handleAction(m)}
-                                        />
-                                    </Table.Cell>
-                                </Table.Row>
-                            )
-                        }
-                    </Table.Body>
-                </Table>
-            </div>
-        )
-    }
-
-    private handleAction = (m: BasicEntityNode) => (act: (payload?: {}) => void, action: string) => {
-        if (action === ENTITY_VIEW) {
-            act({
-                datasetId: this.props.datasetId,
-                entityId: m.id
-            })
-        } else if (action === MENTION_SEARCH) {
-            act({
-                datasetId: this.props.datasetId,
-                type: m.type,
-                value: m.value
-            })
-        } else if (action === ENTITY_SEARCH) {
-            act({
-                datasetId: this.props.datasetId,
-                type: m.type,
-                value: m.value
-            })
+  private handleAction = (m: BasicEntityNode) => (act: (payload?: {}) => void, action: string) => {
+    if (action === ENTITY_VIEW) {
+      act({
+        datasetId: this.props.datasetId,
+        entityId: m.id
+      })
+    } else if (action === MENTION_SEARCH) {
+      act({
+        datasetId: this.props.datasetId,
+        mentionFilter: {
+          type: m.type,
+          value: m.value
         }
+      })
+    } else if (action === ENTITY_SEARCH) {
+      act({
+        datasetId: this.props.datasetId,
+        entityFilter: {
+          type: m.type,
+          value: m.value
+        }
+      })
     }
+  }
 }
 
 export default class PaginatedPropertiesTable extends React.Component<Props> {
-    render() {
-        return (
-            <Paginated items={this.props.entities} itemsKey="entities">
-                <EntityTable
-                    {...this.props}
-                />
-            </Paginated>
-        )
-    }
+  render() {
+    return (
+      <Paginated items={this.props.entities} itemsKey="entities">
+        <EntityTable {...this.props} />
+      </Paginated>
+    )
+  }
 }
--- packages/ketos-components/src/ketos/mentions/MentionTable.tsx
+++ packages/ketos-components/src/ketos/mentions/MentionTable.tsx
@@ -1,103 +1,90 @@
+import { ActionDropdown, Paginated } from 'invest-components'
 import * as React from 'react'
 import { Table } from 'semantic-ui-react'
-import { ActionDropdown } from 'invest-components'
+import { ENTITY_VIEW, MENTION_SEARCH, MENTION_VIEW, RELATION_SEARCH } from '../../Actions'
 import { Mention } from '../../types'
-import { MENTION_SEARCH, MENTION_VIEW, ENTITY_VIEW, RELATION_SEARCH } from '../../Actions'
-import { Paginated } from 'invest-components'
 
 export interface OwnProps {
-    datasetId: string,
-    mentions: Mention[]
-    selectedMentions?: string[]
+  datasetId: string
+  mentions: Mention[]
+  selectedMentions?: string[]
 }
 
 export type Props = OwnProps
 
 class MentionTable extends React.Component<Props> {
+  render() {
+    const { mentions, selectedMentions } = this.props
 
-    render() {
-        const { mentions, selectedMentions } = this.props
+    const selected = selectedMentions != null ? selectedMentions : []
 
-        const selected = selectedMentions != null ? selectedMentions : []
+    return (
+      <div>
+        <Table>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>Type</Table.HeaderCell>
+              <Table.HeaderCell>Value</Table.HeaderCell>
+              <Table.HeaderCell />
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {mentions.map(m => (
+              <Table.Row key={m.id} positive={selected.includes(m.id)}>
+                <Table.Cell>{m.type}</Table.Cell>
+                <Table.Cell>{m.value}</Table.Cell>
+                <Table.Cell>
+                  <ActionDropdown
+                    text="View"
+                    actions={[MENTION_VIEW, ENTITY_VIEW, MENTION_SEARCH, RELATION_SEARCH]}
+                    onSelect={this.handleMentionAction(m)}
+                  />
+                </Table.Cell>
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </Table>
+      </div>
+    )
+  }
 
-        return (
-            <div>
-                <Table>
-                    <Table.Header>
-                        <Table.Row>
-                            <Table.HeaderCell>
-                                Type
-                            </Table.HeaderCell>
-                            <Table.HeaderCell>
-                                Value
-                            </Table.HeaderCell>
-                            <Table.HeaderCell />
-                        </Table.Row>
-                    </Table.Header>
-                    <Table.Body>
-                        {
-                            mentions.map(m =>
-                                <Table.Row key={m.id} positive={selected.includes(m.id)} >
-                                    <Table.Cell>
-                                        {m.type}
-                                    </Table.Cell>
-                                    <Table.Cell>
-                                        {m.value}
-                                    </Table.Cell>
-                                    <Table.Cell>
-                                        <ActionDropdown
-                                            text="View"
-                                            actions={[MENTION_VIEW, ENTITY_VIEW,
-                                                MENTION_SEARCH, RELATION_SEARCH]}
-                                            onSelect={this.handleMentionAction(m)}
-                                        />
-                                    </Table.Cell>
-                                </Table.Row>
-                            )
-                        }
-                    </Table.Body>
-                </Table>
-            </div>
-        )
-    }
-
-    private handleMentionAction = (m: Mention) => (act: (payload?: {}) => void, action: string) => {
-        if (action === MENTION_VIEW) {
-            act({
-                datasetId: this.props.datasetId,
-                mentionId: m.id
-            })
-        } else if (action === ENTITY_VIEW && m.entityId) {
-            act({
-                datasetId: this.props.datasetId,
-                entityId: m.entityId
-            })
-        } else if (action === MENTION_SEARCH) {
-            act({
-                datasetId: this.props.datasetId,
-                type: m.type,
-                value: m.value
-            })
-        } else if (action === RELATION_SEARCH) {
-            act({
-                datasetId: this.props.datasetId,
-                source: {
-                    type: m.type,
-                    value: m.value
-                }
-            })
+  private handleMentionAction = (m: Mention) => (act: (payload?: {}) => void, action: string) => {
+    if (action === MENTION_VIEW) {
+      act({
+        datasetId: this.props.datasetId,
+        mentionId: m.id
+      })
+    } else if (action === ENTITY_VIEW && m.entityId) {
+      act({
+        datasetId: this.props.datasetId,
+        entityId: m.entityId
+      })
+    } else if (action === MENTION_SEARCH) {
+      act({
+        datasetId: this.props.datasetId,
+        mentionFilter: {
+          type: m.type,
+          value: m.value
         }
+      })
+    } else if (action === RELATION_SEARCH) {
+      act({
+        datasetId: this.props.datasetId,
+        source: {
+          type: m.type,
+          value: m.value
+        }
+      })
     }
+  }
 }
 
 export default class PaginatedMentionTable extends React.Component<Props> {
-    render() {
-        return (
-            <Paginated items={this.props.mentions} itemsKey="mentions">
-                <MentionTable
-                    {...this.props}
-                />
-            </Paginated>
-        )
-    }
+  render() {
+    return (
+      <Paginated items={this.props.mentions} itemsKey="mentions">
+        <MentionTable {...this.props} />
+      </Paginated>
+    )
+  }
 }
